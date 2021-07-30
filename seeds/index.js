12b91986f3a272e4960688cb98148d665ca5cf7e
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-07-29 21:50:24 -0700

    add seed data

--- seeds/index.js
+++ seeds/index.js
@@ -1,6 +1,9 @@
 //require Models //! If using JSON files for seeds, otherwise require Models in the seed.js file
-
+const { Blog, User, Comment } = require("../models"); 
 //require seeds (see MVC 07 instructor)
+const blogData = require("./blogData.json");
+const commentData = require("./commentData.json");
+const userData = require("./userData.json");
 
 const sequelize = require('../config/connection');
 
@@ -8,11 +11,14 @@ const seedDatabase = async () => {
     await sequelize.sync({ force: true });
     
     // TODO: Add bulk creates for every 
-    // await [ADD MODEL HERE].bulkCreate(dishData, {
-    //   individualHooks: true,
-    //   returning: true,
-    // });
-  
+    await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+    
+    //add commentData
+    //add blogData
+
     process.exit(0);
   };
   
