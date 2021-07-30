99a5d6250f5d3792a348b4c372bd0de414d0162b
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-07-29 20:35:12 -0700

    add models, set foreign key connectiosn in model index

--- models/index.js
+++ models/index.js
@@ -1,7 +1,39 @@
 // import models
-
+const { Blog } = require("./blog");
+const { User } = require("./user");
+const { Comment } = require("./comment");
 //Connections between tables
+
+Blog.hasOne(User, {
+    foreignKey: 'user_id',
+});
+
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE',
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'SET NULL',
+});
+
+Comment.hasOne(User, {
+    foreignKey: 'user_id',
+});
+
+Comment.hasOne(Blog, {
+    foreignKey: 'blog_id',
+})
 
 module.exports = {
     //Models go here
+    Blog,
+    User,
+    Comment
 }
