73166728d6ea1458b5ce10b6e9678df75f243b84
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-07-30 17:37:55 -0700

    add routing files, start public/js, add checkpassword in models user.js

---
+++ controllers/home-routes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { Blog } = require('../models');
+const withAuth = require('../utils/auth');
+
+// GET all blogs for homepage
+router.get('/', async (req, res) => {
+    try {
+        const blogData = await Blog.findAll({})
+        const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+        res.render('home', {
+            blogs,
+            loggedIn: req.session.loggedIn,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+});
+
+// GET one blog
+
+//Return to homepage if already logged in and somehow user goes to /login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
+    }
+    res.render('login');
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,14 +1,16 @@
 const router = require('express').Router()
 const apiRoutes = require('./api')
+const homeRoutes = require('./home-routes');
 
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
-router.get('/', async (req, res) => {
-    try {
-        res.status(200).render('test');
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         res.status(200).render('test');
+//     } catch (err) {
+//         res.status(500).json(err);
+//     }
+// });
 
 module.exports = router;
