3513f05dcfa10a46fa6513fba9934a12d83d0130
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-02 20:25:33 -0700

    add frontend script to create new post at selfCreate.hb

--- public/js/createPost.js
+++ public/js/createPost.js
@@ -1,5 +1,24 @@
 //TODO: post fetch to /dashboard/add
 
+const addPost = async (event) => {
+    event.preventDefault();
 
+    const title = document.querySelector('#new-post-title').value.trim();
+    const content = document.querySelector('#new-post-content').value.trim();
 
-document.querySelector('.addPostBtn').addEventListener('click', addPost)
+    if (title && content) {
+        const response = await fetch ('/dashboard/add', {
+            method: 'POST',
+            body: JSON.stringify({ title, content}),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert ('Failed to create new blogpost');
+        }
+    }
+}
+
+document.querySelector('.addPostBtn').addEventListener('submit', addPost)
