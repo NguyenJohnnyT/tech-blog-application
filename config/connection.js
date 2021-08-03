2f4363fd3819d35a5a4e9c61423bfd843badfde3
NguyenJohnnyT <johnnytrucnguyen@gmail.com>
2021-08-02 23:59:05 -0700

    connect to jawsdb fix

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+let sequelize
+
 if (process.env.JAWSDB_URL) {
-  const sequelize = new Sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  const sequelize = new Sequelize(
+  sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
